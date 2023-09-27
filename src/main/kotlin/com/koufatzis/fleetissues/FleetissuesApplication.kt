641f7e78bdf2682495da83a9f725f957d3330891
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2023-09-27 11:33:52 +0300

    Initial commit

---
+++ src/main/kotlin/com/koufatzis/fleetissues/FleetissuesApplication.kt
@@ -0,0 +1,11 @@
+package com.koufatzis.fleetissues
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FleetissuesApplication
+
+fun main(args: Array<String>) {
+	runApplication<FleetissuesApplication>(*args)
+}
---
+++ src/test/kotlin/com/koufatzis/fleetissues/FleetissuesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.koufatzis.fleetissues
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FleetissuesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
