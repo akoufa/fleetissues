2526f4246f3e6aff462ba4a8395d6e3179a178cd
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2024-04-17 23:57:32 +0300

    Update Fleet issues with avro issue

--- src/main/kotlin/com/koufatzis/fleetissues/FleetissuesApplication.kt
+++ src/main/kotlin/com/koufatzis/fleetissues/FleetissuesApplication.kt
@@ -1,5 +1,6 @@
 package com.koufatzis.fleetissues
 
+import com.koufatzis.gen.fleet.Fleet
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class FleetissuesApplication
 
 fun main(args: Array<String>) {
+	// This is the generated class from the Avro file
+	Fleet()
 	runApplication<FleetissuesApplication>(*args)
 }
