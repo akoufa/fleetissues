8f2742e949bb696274a36f11d4d824766569ae47
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2024-08-13 18:42:25 +0300

    Update kotlin version

---
+++ src/test/kotlin/com/koufatzis/fleetissues/FleetIssuesJUnit.kt
@@ -0,0 +1,15 @@
+package com.koufatzis.fleetissues
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+
+class FleetIssuesJUnit {
+    
+    @Test
+    fun test() {
+        assertTrue(true)
+    }
+    
+}
+
